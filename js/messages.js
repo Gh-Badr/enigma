d7e7edcc04b01b51acae243a5d575f0cd23bad73
Badr GHAZAOUI <ghazaoui.badr.tech@gmail.com>
2023-06-07 22:06:57 +0100

    Clean up code

--- js/messages.js
+++ js/messages.js
@@ -1,20 +1,17 @@
-
 let messageContainer = document.querySelector(".message-container");
 
-function displayMessage(type,message=null){
-    if(type=="error"){
-        messageContainer.innerHTML=message;
-        messageContainer.classList.remove("warning-message","valid-message");
-        messageContainer.classList.add("error-message");
-    }
-    else if(type=="warning"){
-        messageContainer.innerHTML=message;
-        messageContainer.classList.remove("error-message","valid-message");
-        messageContainer.classList.add("warning-message");
-    }
-    else {
-        messageContainer.innerHTML="The code is valid !";
-        messageContainer.classList.remove("warning-message","error-message");
-        messageContainer.classList.add("valid-message");
-    }
+function displayMessage(type, message = null) {
+  if (type == "error") {
+    messageContainer.innerHTML = message;
+    messageContainer.classList.remove("warning-message", "valid-message");
+    messageContainer.classList.add("error-message");
+  } else if (type == "warning") {
+    messageContainer.innerHTML = message;
+    messageContainer.classList.remove("error-message", "valid-message");
+    messageContainer.classList.add("warning-message");
+  } else {
+    messageContainer.innerHTML = "The code is valid !";
+    messageContainer.classList.remove("warning-message", "error-message");
+    messageContainer.classList.add("valid-message");
+  }
 }
